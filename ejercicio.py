37b554c386b26937a45af51f322a41382ae4ec87
Santiago Ramon <santiramon967@gmail.com>
2024-09-07 17:45:47 -0500

    cambios menores

--- ejercicio.py
+++
@@ -1,7 +0,0 @@
-from sklearn.metrics.pairwise import cosine_similarity
-
-import numpy as np
-vec1 = np.array ([[1,0,1,2,0]])
-vec2 = np.array ([[0,1,1,1,2]])
-vec3 = np.array ([[1,1,0,2,0]])
-print ('Similaridad entre V1 y V2 \n', cosine_similarity (vec1, vec2))
